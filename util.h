85eba67063065f5c5e11ab93e2749546532bffa6
Kenny Liu <nike9926@gmail.com>
2016-03-16 18:45:51 -0700

    Collect all necessary standard header files into util.h

---
+++ util.h
@@ -0,0 +1,11 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+/* Put all necessary standard header files here */
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+#endif /* __UTIL_H__ */
